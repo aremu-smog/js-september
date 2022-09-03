7d59f86603118208aed5456f52011000adbf679e
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-03 21:24:12 +0100

    feat: add color options to the popup

--- 01-chrome-extension-tutorial/background.js
+++ 01-chrome-extension-tutorial/background.js
@@ -1,6 +1,7 @@
 let color = "#3aa757"
 
 chrome.runtime.onInstalled.addListener(() => {
+	document.querySelector("body").style.backgroundColor
 	chrome.storage.sync.set({ color })
 	console.log("Default background color set to %cgreen", `Color : ${color}`)
 })
--- 01-chrome-extension-tutorial/popup.js
+++ 01-chrome-extension-tutorial/popup.js
@@ -1,4 +1,5 @@
 const changeColorButton = window.document.querySelector("#changeColor")
+const resetColorButton = window.document.querySelector("#resetColor")
 
 chrome.storage.sync.get("color", ({ color }) => {
 	changeColorButton.style.backgroundColor = color
@@ -13,8 +14,54 @@ changeColorButton.addEventListener("click", async () => {
 	})
 })
 
+resetColorButton.addEventListener("click", async () => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		func: resetBackgroundColor,
+	})
+})
+
+const resetBackgroundColor = () => {
+	document.body.style.removeProperty("background-color")
+}
+
 const setPageBackgroundColor = () => {
 	chrome.storage.sync.get("color", ({ color }) => {
 		document.body.style.backgroundColor = color
 	})
 }
+
+const colorOptions = [
+	{ label: "Green", value: "#3aa757" },
+	{ label: "Red", value: "#e8453c" },
+	{ label: "Yellow", value: "#f9bb2d" },
+	{ label: "Blue", value: "#4688f1" },
+]
+
+const colorOptionsSelect = document.querySelector("#colorOptions")
+
+colorOptions.forEach(colorOption => {
+	const { label, value } = colorOption
+	const selectOption = document.createElement("option")
+	selectOption.value = value
+	selectOption.innerText = label
+
+	colorOptionsSelect.append(selectOption)
+
+	chrome.storage.sync.get("color", ({ color }) => {
+		if (color === value) {
+			selectOption.setAttribute("selected", true)
+		} else {
+			selectOption.removeAttribute("selected")
+		}
+	})
+})
+
+colorOptionsSelect.addEventListener("change", e => {
+	const selectedColor = e.target.value
+	chrome.storage.sync.set({ color: selectedColor })
+
+	changeColorButton.style.backgroundColor = selectedColor
+})
