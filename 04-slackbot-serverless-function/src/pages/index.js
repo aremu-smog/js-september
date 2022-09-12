287baae51a60221a2450904857070d717c107d40
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-12 20:10:13 +0100

    feat: handle states on the frontendt

--- 04-slackbot-serverless-function/src/pages/index.js
+++ 04-slackbot-serverless-function/src/pages/index.js
@@ -1,5 +1,142 @@
 import * as React from "react"
 
+const IndexPage = () => {
+	const [contactData, setContactData] = React.useState({})
+	const [formState, setFormState] = React.useState({
+		status: "",
+		message: "",
+	})
+
+	const IS_LOADING = formState.status === "LOADING"
+	const IS_ERROR = formState.status === "ERROR"
+	const IS_SUCCESS = formState.status === "SUCCESS"
+
+	const handleChange = e => {
+		const { name, value } = e.target
+
+		setContactData(prevContactData => {
+			return { ...prevContactData, [name]: value }
+		})
+	}
+	const sendMessageToSlack = async e => {
+		e.preventDefault()
+		setFormState(currentFormState => {
+			return { ...currentFormState, status: "LOADING" }
+		})
+		try {
+			await fetch("/api/slackbot", {
+				method: "POST",
+				headers: {
+					"content-type": "application/json",
+				},
+				body: JSON.stringify(contactData),
+			})
+				.then(res => res.json())
+				.then(data => {
+					setFormState(currentFormState => {
+						return {
+							...currentFormState,
+							status: "SUCCESS",
+							message: "Someone on our team will get in touch with you soon!",
+						}
+					})
+				})
+				.catch(e => {
+					setFormState(currentFormState => {
+						return {
+							...currentFormState,
+							status: "ERROR",
+							message: "Something went terribly wrong",
+						}
+					})
+				})
+		} catch {
+			alert("Another gbege")
+		} finally {
+			setFormState(currentFormState => {
+				return { ...currentFormState, status: "" }
+			})
+		}
+	}
+	return (
+		<main style={pageStyles}>
+			<h1 style={headingStyles}>Oniṣowo</h1>
+
+			<form onSubmit={sendMessageToSlack}>
+				<div>
+					<label style={formLabelStyles} htmlFor='fullname'>
+						Fullname {contactData?.fullname}
+					</label>
+					<input
+						type='text'
+						placeholder='Your full name'
+						name='fullname'
+						id='fullname'
+						style={formInputStyles}
+						value={contactData?.fullname}
+						onChange={handleChange}
+						required='required'
+					/>
+				</div>
+				<div>
+					<label style={formLabelStyles} htmlFor='company'>
+						Company
+					</label>
+					<input
+						type='text'
+						placeholder='Your Company'
+						name='company'
+						id='company'
+						style={formInputStyles}
+						onChange={handleChange}
+						value={contactData?.company}
+						required='required'
+					/>
+				</div>
+				<div>
+					<label style={formLabelStyles} htmlFor='email'>
+						Email Address
+					</label>
+					<input
+						type='email'
+						placeholder='Your Email Address'
+						name='email'
+						id='email'
+						style={formInputStyles}
+						onChange={handleChange}
+						value={contactData?.email}
+						required='required'
+					/>
+				</div>
+				<div>
+					<label style={formLabelStyles} htmlFor='message'>
+						Your message
+					</label>
+					<textarea
+						placeholder="Don't hold anything back..."
+						name='message'
+						id='message'
+						style={formInputStyles}
+						onChange={handleChange}
+						value={contactData?.message}
+						required='required'
+					/>
+				</div>
+				<button style={formButtonStyles} disabled={IS_LOADING}>
+					{IS_LOADING ? "Loading..." : "Submit"}
+				</button>
+			</form>
+
+			<img
+				alt='Gatsby G Logo'
+				src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+			/>
+		</main>
+	)
+}
+
+export default IndexPage
+
 const pageStyles = {
 	color: "#232129",
 	padding: 96,
@@ -103,95 +240,3 @@ const formButtonStyles = {
 	border: "none",
 	width: "200px",
 }
-const IndexPage = () => {
-	const sendMessageToSlack = async e => {
-		e.preventDefault()
-		try {
-			await fetch("/api/slackbot", {
-				method: "POST",
-				headers: {
-					"content-type": "application/json",
-				},
-				body: JSON.stringify({
-					text: `Hello World again ${Date.now()}`,
-				}),
-			})
-				.then(res => res.json())
-				.then(data => {
-					alert("E don show")
-				})
-				.catch(e => {
-					alert("Error o", e)
-				})
-		} catch {
-			alert("Another gbege")
-		}
-	}
-	return (
-		<main style={pageStyles}>
-			<h1 style={headingStyles}>Oniṣowo</h1>
-
-			<p style={paragraphStyles}>
-				<form onSubmit={sendMessageToSlack}>
-					<div>
-						<label style={formLabelStyles} htmlFor='fullname'>
-							Fullname
-						</label>
-						<input
-							type='text'
-							placeholder='Your full name'
-							name='fullname'
-							id='fullname'
-							style={formInputStyles}
-						/>
-					</div>
-					<div>
-						<label style={formLabelStyles} htmlFor='company'>
-							Company
-						</label>
-						<input
-							type='text'
-							placeholder='Your Company'
-							name='company'
-							id='company'
-							style={formInputStyles}
-						/>
-					</div>
-					<div>
-						<label style={formLabelStyles} htmlFor='email'>
-							Email Address
-						</label>
-						<input
-							type='email'
-							placeholder='Your Email Address'
-							name='email'
-							id='email'
-							style={formInputStyles}
-						/>
-					</div>
-					<div>
-						<label style={formLabelStyles} htmlFor='message'>
-							Your message
-						</label>
-						<textarea
-							placeholder="Don't hold anything back..."
-							name='message'
-							id='message'
-							style={formInputStyles}
-						/>
-					</div>
-					<button style={formButtonStyles}>Get me to slack</button>
-				</form>
-			</p>
-
-			<img
-				alt='Gatsby G Logo'
-				src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-			/>
-		</main>
-	)
-}
-
-export default IndexPage
-
-export const Head = () => <title>Home Page</title>
