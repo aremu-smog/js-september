fb3ea4baa8aa7d7fc710ec9d0610ed7b2d4bf1c8
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-13 21:55:27 +0100

    feat: setup for pomodoro extension

---
+++ 05-pomodoro-chrome-extension/pomodoro.js
@@ -0,0 +1,7 @@
+const pomodoroForm = document.querySelector("#pomodoro")
+
+pomodoroForm.addEventListener("submit", e => {
+	e.preventDefault()
+
+	alert("Look who is taking their life serious")
+})
