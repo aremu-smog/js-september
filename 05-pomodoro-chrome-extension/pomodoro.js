b5f486ecb11c6c71ac4a534462a5e3373379bd1b
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-15 22:15:18 +0100

    feat: finish up ui and disable submit button

--- 05-pomodoro-chrome-extension/pomodoro.js
+++ 05-pomodoro-chrome-extension/pomodoro.js
@@ -1,21 +1,23 @@
 const pomodoroForm = document.querySelector("#pomodoro")
 const workTimeInput = document.querySelector("#workTime")
 const breakTimeInput = document.querySelector("#breakTime")
+const submitButton = document.querySelector("#submitButton")
 const resetButton = document.querySelector("#resetButton")
 
-pomodoroForm.addEventListener("submit", e => {
+pomodoroForm.addEventListener("submit", async e => {
 	e.preventDefault()
 
-	console.log("Start up!")
-
-	chrome.alarms.create("workTime", {
+	await chrome.alarms.create("workTime", {
 		periodInMinutes: workTimeInput.valueAsNumber,
 	})
-	chrome.alarms.create("breakTime", {
+	await chrome.alarms.create("breakTime", {
 		periodInMinutes: breakTimeInput.valueAsNumber + workTimeInput.valueAsNumber,
 	})
+
+	submitButton.setAttribute("disabled", "disabled")
 })
 
 resetButton.addEventListener("click", e => {
+	submitButton.removeAttribute("disabled")
 	chrome.alarms.clearAll()
 })
