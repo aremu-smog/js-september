b703735e3ee474361ed65bc8d08890eeb4f6c092
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-19 23:36:06 +0100

    ao_feat: select voices

--- 05-pomodoro-chrome-extension/alarm.js
+++ 05-pomodoro-chrome-extension/alarm.js
@@ -1,27 +1,33 @@
 chrome.alarms.onAlarm.addListener(alarm => {
-	console.log(alarm)
-
 	const { name } = alarm
 
 	if (name === "breakTime") {
 		chrome.alarms.clear("breakTime", () => {
-			chrome.tts.speak("Time to get back to work!")
-
-			chrome.storage.sync.get("workTime", ({ workTime }) => {
-				chrome.alarms.create("workTime", {
-					periodInMinutes: workTime,
-				})
-			})
+			chrome.storage.sync.get(
+				["workTime", "voice"],
+				async ({ workTime, voice }) => {
+					await chrome.tts.speak("Time to get back to work!", {
+						voiceName: voice,
+					})
+					chrome.alarms.create("workTime", {
+						periodInMinutes: workTime,
+					})
+				}
+			)
 		})
 	} else if (name === "workTime") {
 		chrome.alarms.clear("workTime", () => {
-			chrome.tts.speak("Time to take a break!")
-
-			chrome.storage.sync.get("breakTime", ({ breakTime }) => {
-				chrome.alarms.create("breakTime", {
-					periodInMinutes: breakTime,
-				})
-			})
+			chrome.storage.sync.get(
+				["breakTime", "voice"],
+				async ({ breakTime, voice }) => {
+					await chrome.tts.speak("Time to take a break!", {
+						voiceName: voice,
+					})
+					chrome.alarms.create("breakTime", {
+						periodInMinutes: breakTime,
+					})
+				}
+			)
 		})
 	}
 })
--- 05-pomodoro-chrome-extension/pomodoro.js
+++ 05-pomodoro-chrome-extension/pomodoro.js
@@ -1,18 +1,23 @@
 const pomodoroForm = document.querySelector("#pomodoro")
 const workTimeInput = document.querySelector("#workTime")
 const breakTimeInput = document.querySelector("#breakTime")
+const voiceInput = document.querySelector("#voice")
 const submitButton = document.querySelector("#submitButton")
 const resetButton = document.querySelector("#resetButton")
 
 chrome.storage.sync.get(
-	["workTime", "breakTime"],
-	({ workTime, breakTime }) => {
+	["workTime", "breakTime", "voice"],
+	({ workTime, breakTime, voice }) => {
 		if (workTime && breakTime) {
 			workTimeInput.value = workTime
 			breakTimeInput.value = breakTime
 			workTimeInput.setAttribute("disabled", "disabled")
 			breakTimeInput.setAttribute("disabled", "disabled")
 			submitButton.setAttribute("disabled", "disabled")
+		}
+		if (voice) {
+			voiceInput.value = voice
+			voiceInput.setAttribute("disabled", "disabled")
 		}
 	}
 )
@@ -22,6 +27,10 @@ pomodoroForm.addEventListener("submit", async e => {
 
 	await chrome.storage.sync.set({ workTime: workTimeInput.valueAsNumber })
 	await chrome.storage.sync.set({ breakTime: workTimeInput.valueAsNumber })
+	if (voiceInput.value) {
+		await chrome.storage.sync.set({ voice: voiceInput.value })
+		voiceInput.setAttribute("disabled", "disabled")
+	}
 
 	await chrome.storage.sync.get("workTime", ({ workTime }) => {
 		chrome.alarms.create("workTime", {
@@ -37,9 +46,13 @@ pomodoroForm.addEventListener("submit", async e => {
 resetButton.addEventListener("click", async e => {
 	await chrome.storage.sync.set({ workTime: 0 })
 	await chrome.storage.sync.set({ breakTime: 0 })
+	await chrome.storage.sync.set({ voice: "" })
 	breakTimeInput.value = ""
 	workTimeInput.value = ""
+	voiceInput.value = ""
+
 	breakTimeInput.removeAttribute("disabled")
+	voiceInput.removeAttribute("disabled")
 	workTimeInput.removeAttribute("disabled")
 	submitButton.removeAttribute("disabled")
 	chrome.alarms.clearAll()
